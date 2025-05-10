f49caccb5bb73af662515bd39f37055d0a1a5a5a
Christian Petruzzella <135434850+christianp12@users.noreply.github.com>
2025-05-10 14:57:52 +0200

    get_random_question

---
+++ agents/question_agent/agent.py
@@ -0,0 +1,18 @@
+from google.adk.agents import Agent
+import random
+import csv
+
+
+def get_random_question():
+    with open('agents/question_agent/qanda.csv', 'r') as file:
+        questions = [row[0] for row in csv.reader(file)]
+    return random.choice(questions)
+
+
+root_agent = Agent(
+    model='gemini-2.0-flash-001',
+    name='root_agent',
+    description='An agents that retrieves a question from a set of questions',
+    instruction='Provide the user a random question picked from a list of questions using the get_question tool',
+    tools=[get_random_question]
+)
